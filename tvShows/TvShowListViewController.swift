7afc910f6cad7a45d6760f098994386bd96cb826
teddy <teddyjh@protonmail.com>
2022-09-19 17:39:40 +0200

    Add plan & add table view+nav controller

---
+++ tvShows/TvShowListViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  tvShows
+//
+//  Created by teddy on 2022-09-19.
+//
+
+import UIKit
+
+class TvShowListViewController: UITableViewController {
+    // Plan:
+    // Create a UITableView (to show items in a list)
+    // Embed in a navigation controller
+    // Create a DetailView for the items in the list
+    // Create an ApiCaller for TV-Maze API (https://www.tvmaze.com/api)
+    //      Test with api endpoint: https://api.tvmaze.com/singlesearch/shows?q=girls
+    // Create a data model for the data from
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        title = "TV Shows"
+    }
+
+
+}
+
