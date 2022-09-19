3eed9587a6b0b64db6adad990a18a6de45d9b1d5
teddy <teddyjh@protonmail.com>
2022-09-19 17:51:38 +0200

    Add tv show detail view controller class (start)

---
+++ tvShows/TvShowDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TvShowDetailViewController.swift
+//  tvShows
+//
+//  Created by teddy on 2022-09-19.
+//
+
+import UIKit
+
+class TvShowDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
