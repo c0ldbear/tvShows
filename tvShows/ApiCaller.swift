aff7431c056e22805cad0bf81112ab5d1d86b414
teddy <teddyjh@protonmail.com>
2022-09-20 08:50:59 +0200

    Change to fetch all shows instead of a query

--- tvShows/ApiCaller.swift
+++ tvShows/ApiCaller.swift
@@ -9,16 +9,16 @@ import Foundation
 
 class ApiCaller {
     
-    // Change to: https://api.tvmaze.com/search/shows?q=girls (minus "qirls", this will be added by the search bar)
-    // ^ TODO: Need to change Data model to get "show" as an codable so that we then can get right info
     var tvShowUrlString: String = "https://api.tvmaze.com/shows"
-    var tvShowUrlQuery: String = "https://api.tvmaze.com/search/shows?q="
+//    var tvShowUrlQuery: String = "https://api.tvmaze.com/search/shows?q=" // Could be nice to use if we only want to fetch when user searches
     // get all shows: https://api.tvmaze.com/shows
     
     init() {}
     
     func fetch(with urlQuery: String? = nil) -> [TvShowData]? {
-        if let url = URL(string: urlQuery ?? tvShowUrlString) {
+        var urlString: String = tvShowUrlString
+        
+        if let url = URL(string: urlString) {
             do {
                 let data = try Data(contentsOf: url)
                 return parse(json: data)
@@ -56,12 +56,4 @@ class ApiCaller {
         }
         return nil
     }
-}
-
-// TODO: MOVE TO OWN FILE
-class TvShowData: Codable {
-    var name: String?
-    var language: String?
-    var genres: [String]?
-    var image: [String: String]?
 }
